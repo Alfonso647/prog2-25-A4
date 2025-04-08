187f7879a4d80ee34ee7679d9f6922a0d088988d
Marcos Hernández Juárez <mhj7@alu.ua.es>
2025-04-08 23:39:12 +0200

    main y cambios
    He implementado un nuevo archivo "main" que sustituye al anterior (que estaba de prueba, solamente era para ver funciones) y con él he hecho un menú para poder ir seleccionando.
    No está terminado porque creo que para una parte es necesaria la API.
    
    También he cambiado el archivo de TCliente para añadir una opción en la que no haya historial de compras en su método correspondiente.
    
    Por último he separado las clases de Producto y Tienda en dos archivos distintos, ya que estaban en el mismo y daba error al separarlos.

---
+++ TProd.py
@@ -0,0 +1,57 @@
+
+
+
+class Producto:
+    '''
+    Clase que representa un producto
+    Atributos
+    ----------
+    nombre: str
+        nombre del producto
+    precio: float
+        valor del producto en €
+    stock: int
+        cantidad inicial de unidades del producto
+    volumen: float
+        volumen del procucto en cm cúbicos
+    peso: float
+        peos del producto en gramos
+    fragil: bool
+        indica si el producto es frágil
+
+    Métodos
+    --------
+    __init__
+        inicializa la clase y además añade el producto creado a la tienda online, de manera que siempre que se
+        cree un producto de añadira a la tienda
+
+    '''
+    
+    productos_stock={}
+    productos_precio={}
+
+    def __init__(self, nombre: str,precio: float,stock: int, volumen: float, peso: float, fragil: bool = False):
+        self.nombre = nombre
+        self.volumen = volumen
+        self.peso = peso
+        self.precio = precio
+        self.fragil = fragil
+        self.stock = stock
+
+        self.guardar(self.nombre, self.precio, self.stock)
+        '''
+        Este método guarda el stock y el precio de un producto, ya que luego lo utilizaremos
+        '''
+        
+    def guardar(cls, nombre, precio, stock):
+        cls.productos_stock[nombre] = stock
+        cls.productos_precio[nombre] = precio
+
+        # Tienda().nuevo_producto(self) #cada vez que se crea un producto se guarda en tienda
+
+    def __str__(self) -> str:
+        sn = 'Sí' if self.fragil==True else 'No'
+        cad = f'{self.nombre},{self.precio}€. Tiene un volumen de {self.volumen} centímetros cúbicos y pesa {self.peso} gramos. '
+        cad += f'{sn} es frágil'
+
+        return cad
