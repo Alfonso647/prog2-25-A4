d3ba3193b2e54413790342a890f3c2d04b4bab57
Alfonso647 <aic35@alu.ua.es>
2025-04-09 13:01:15 +0200

    seguridad

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from crypt import methods
-
-from flask import Flask, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
+from flask import Flask, request, jsonify
 import hashlib
 
 'Datos almecenados de nuestra app'
@@ -10,10 +10,36 @@ app = Flask(__name__)
 
 
 app.config['JWT_SECRET_KEY'] = 'A4'
+jwt = JWTManager(app)
+
+@app.route('/singup', methods=['POST'])    #Ininiar sesión
+def singup():
+    user = request.args.get('user', '')
+    if user in users:
+        return f'Usuario {user} ya registrado', 409
+    else:
+        password = request.args.get('password','')
+        hashed = hash.lib.sha256(password.encode()).hexdigest()
+        users[user] = hashed
+        return f'Usuario {user} registrado', 200
+
+###cerrar sesión
+
+#...
+
+@app.route('/singin', methods=['GET']) #Autentificación
+def login():
+    user = request.args.get('user','')
+    password = request.args.get('password','')
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    if user in users and users[user] == hashed:
+        return create_access_token(identity=user), 200
+    else:
+        return  f'Usuario o contraseña incorrectos', 401
 
 @app.route("/")
 def root():
-    return 'hello world!'
+    return 'Api de Productos'
 
 @app.route('/data/<string:id>', methods=['POST'])   #añadir datos
 def add_data(id):
@@ -45,9 +71,6 @@ def delete_data(id):
         return f'Dato {id} eliminado', 200
     else:
         return f'Dato {id} no encontrado'
-
-
-
 
 
 if __name__ == '__main__':
