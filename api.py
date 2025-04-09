6816433ff6af72947e9dc6e91a3bfd02d6b28560
Alfonso647 <aic35@alu.ua.es>
2025-04-09 11:46:10 +0200

    entorno

--- api.py
+++ api.py
@@ -1,5 +1,18 @@
-import flask import Flask,requests,jsonify
+from flask import Flask, requests, jsonify
+#from flask_jwt_extendend import JWTManager, create_acess_token, jwt_required, get_jwt_identity
+#import hashlib
 
+users = {}
+data = {}
 app = Flask(__name__)
-if __name__ = '__main__'
-    app.run(debug = True)
+
+
+app.config['JWT_SECRET_KEY'] = 'A4'
+
+@app.route("/")
+def root():
+    return 'hello world!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
